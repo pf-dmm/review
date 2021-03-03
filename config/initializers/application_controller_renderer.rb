f00f320e8207fe2874a2b36a8b740b1d4af9c8c7
EC2 Default User <ec2-user@ip-172-31-34-194.ap-northeast-1.compute.internal>
2021-03-03 03:58:00 +0000

    kengo-start

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '057bb2a5ffda317b3b8f48df644db7d90122024ac011bd3bed8cc073c82ee3bdf8caa427d96a3262be9d87b0180d38aaee57622376912b1adc8b3766017168c6'
