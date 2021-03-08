377295b2a98940f6c76dd1867acc6f78558df939
EC2 Default User <ec2-user@ip-172-31-45-50.ec2.internal>
2021-03-08 08:54:05 +0000

    [add] review

--- app/controllers/pf_reviews_controller.rb
+++ app/controllers/pf_reviews_controller.rb
@@ -9,28 +9,38 @@ class PfReviewsController < ApplicationController
     @pf_review.reviewer_user_id = current_user.id
     @pf_review.reviewed_user_id = Portfolio.find(params[:pf_review][:portfolio_id]).user_id
     @pf_review.portfolio_id = Portfolio.find(params[:pf_review][:portfolio_id]).id
-    r1 = PfReview.find_by(reviewed_user_id: current_user.id, reviewed_user_id: @pf_review.reviewed_user_id)
-    r2 = PfReview.find_by(reviewed_user_id: @pf_review.reviewed_user_id, reviewed_user_id: current_user.id)
-      if r1 && r2
-        r1.update(review_each_other: true)
-        r2.update(review_each_other: true)
-      end
     if @pf_review.save
+      r1 = PfReview.find_by(reviewed_user_id: current_user.id, reviewer_user_id: @pf_review.reviewed_user_id)
+      r2 = PfReview.find_by(reviewed_user_id: @pf_review.reviewed_user_id, reviewer_user_id: current_user.id)
+        if r1.present? && r2.present?
+          r1.update(review_each_other: true)
+          r2.update(review_each_other: true)
+        end
       redirect_to user_path(current_user)
     else
       @portfolio = Portfolio.find(params[:pf_review][:portfolio_id])
       render "pf_reviews/new"
     end
-
   end
 
   def show
+    @pf_review = PfReview.find(params[:id])
   end
 
   def index
   end
 
   def edit
+    @pf_review = PfReview.find(params[:id])
+  end
+
+  def update
+    @pf_review = PfReview.find(params[:id])
+    if @pf_review.update
+      redirect_to pf_review_path(@pf_review)
+    else
+      render "pf_reviews/edit"
+    end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @review_from_me = PfReview.where(reviewer_user_id: current_user) #自分のPFに対するレビュー
-    @review_to_me = PfReview.where(reviewed_user_id: current_user) #自分がしたレビュー
+    @review_from_me = PfReview.where(reviewer_user_id: current_user) #自分がしたレビュー
+    @review_to_me = PfReview.where(reviewed_user_id: current_user) #自分のPFに対するレビュー
   end
 
   def edit
